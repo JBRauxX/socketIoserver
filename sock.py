8b79c8ceed077f2d1493afc33cc6f90ac163dfc9
JBRauxX <125036571+JBRauxX@users.noreply.github.com>
2023-03-15 13:40:20 +0700

    Add files via upload

---
+++ sock.py
@@ -0,0 +1,25 @@
+import socket, numpy as np
+class socketserver:
+    def __init__(self, address = '', port = 9090):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.address = address
+        self.port = port
+        self.sock.bind((self.address, self.port))
+        self.cummdata = ''
+
+    def recvmsg(self):
+        self.sock.listen(1)
+        self.conn, self.addr = self.sock.accept()
+        print('connected to', self.addr)
+        self.cummdata = ''
+
+        while True:
+            data = self.conn.recv(10000)
+            self.cummdata+=data.decode("utf-8")
+            if not data:
+                break
+            self.conn.send(bytes(calcregr(self.cummdata), "utf-8"))
+            return self.cummdata
+
+    def __del__(self):
+        self.sock.close()
